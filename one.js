9ec6cd5e7b61c9368544bf560682c3aefae17e76
zahra <zahra_zb1@hotmail.com>
2017-02-19 13:41:00 +0200

    edit2

--- one.js
+++ one.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function(req,res){
-   res.end("<h1>welcome..<h1>");
+   res.end("<h1 style="color: #ffec7a;font-weight: bold;font-size: 70px;margin: 70px 0px 20px;">welcome..<h1>");
  }).listen(process.env.PORT || 8080,function(){
     console.log('Listening on 8080');
   });
