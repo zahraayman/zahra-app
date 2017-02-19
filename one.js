55736d334bfead0cc901697a6f4a0449ff9a7678
zahra <zahra_zb1@hotmail.com>
2017-02-19 13:27:20 +0200

    edit

--- one.js
+++ one.js
@@ -1,8 +1,6 @@
 var http = require('http');
-var fs = require('fs');
-var result = fs.readFileSync(__dirname +'/index.html' ,'utf8');
 http.createServer(function(req,res){
-   res.end(result);
+   res.end("welcome");
  }).listen(process.env.PORT || 8080,function(){
     console.log('Listening on 8080');
   });
