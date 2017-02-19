be9b39fa18ef72375025d28c12484e8f08764239
zahra <zahra_zb1@hotmail.com>
2017-02-19 13:18:23 +0200

    heroku mod

--- one.js
+++ one.js
@@ -1,9 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var result = fs.readFileSync(__dirname +'/index.html' ,'utf8');
-var css = fs.readFileSync(__dirname +'/style.css');
 http.createServer(function(req,res){
    res.end(result);
- }).listen(8080,function(){
+ }).listen(process.env.PORT || 8080,function(){
     console.log('Listening on 8080');
   });
